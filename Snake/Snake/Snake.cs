4d77b8b78a336086f6a66f31073ee8b51dd53130
fantest <fanwarriors@yandex.ru>
2015-11-18 03:39:37 +0300

    Snake move class

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -7,8 +7,12 @@ namespace Snake
 {
     class Snake :Figure
     {
-        public Snake(Point tail, int lenght, Direction direction)
+        Direction direction;
+
+        public Snake(Point tail, int lenght, Direction _direction)
         {
+            direction = _direction;
+
             pList = new List<Point>();
             for (int i = 0; i < lenght; i++)
             {
@@ -16,6 +20,26 @@ namespace Snake
                 p.Move(i, direction);
                 pList.Add( p );
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove (tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+            
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move (1, direction);
+            return nextPoint;
         }
     }
 }
